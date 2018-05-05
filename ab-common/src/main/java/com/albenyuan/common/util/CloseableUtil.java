6e73afd6f4a96da2a460a43c76d3c3c2241e68e3
Alben Yuan <albenyuan@aliyun.com>
2018-05-05 12:10:29 +0800

    CloseableUtil

---
+++ ab-common/src/main/java/com/albenyuan/common/util/CloseableUtil.java
@@ -0,0 +1,30 @@
+package com.albenyuan.common.util;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+/**
+ * @Author Alben Yuan
+ * @Date 2017-12-25 13:57
+ */
+
+public class CloseableUtil {
+
+    public static void close(Closeable... closeables) {
+        for (int i = 0; i < closeables.length; i++) {
+            close(closeables[i]);
+        }
+    }
+
+    public static void close(Closeable closeable) {
+        try {
+            if (closeable != null) {
+                closeable.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            closeable = null;
+        }
+    }
+}
