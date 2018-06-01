985c70d2adeb61158c3e498b0afc4340724f5b1d
Alben Yuan <albenyuan@aliyun.com>
2018-06-01 15:32:42 +0800

    特殊字符

---
+++ ab-common/src/main/java/com/albenyuan/common/constant/CharConstant.java
@@ -0,0 +1,631 @@
+package com.albenyuan.common.constant;
+
+/**
+ * @Author Alben Yuan
+ * @Date 2018-01-29 17:10
+ */
+
+public class CharConstant {
+
+    /**
+     * 中划线
+     */
+    public static final String HYPHEN = "-";
+
+    /**
+     * 问号
+     */
+    public static final String QUESTION = "?";
+
+    /**
+     * 逗号
+     */
+    public static final String COMMA = ",";
+
+    /**
+     * 点
+     */
+    public static final String DOT = ".";
+
+    /**
+     * 冒号
+     */
+    public static final String COLON = ":";
+
+    /**
+     * 分号
+     */
+    public static final String SEMICOLON = ";";
+
+    /**
+     * 双引号
+     */
+    public static final String QUOTE_DOUBLE = "\"";
+
+    /**
+     * 单引号
+     */
+    public static final String QUOTE_SINGLE = "'";
+
+    /**
+     *
+     */
+    public static final String GRAVE_ACCENT = "`";
+
+    /**
+     *
+     */
+    public static final String ASTERISK = "*";
+
+    /**
+     * 加号
+     */
+    public static final String PLUS = "+";
+
+    /**
+     * 减号
+     */
+    public static final String DASH = HYPHEN;
+
+    /**
+     *
+     */
+    public static final String MINUS = HYPHEN;
+
+    /**
+     *
+     */
+    public static final String EQUAL = "=";
+
+    /**
+     *
+     */
+    public static final String SLASH = "/";
+
+    /**
+     *
+     */
+    public static final String BACKSLASH = "\\";
+
+    /**
+     *
+     */
+    public static final String VERTICAL_BAR = "|";
+
+    /**
+     *
+     */
+    public static final String UNDERSCORE = "_";
+
+    /**
+     *
+     */
+    public static final String DOLLAR = "$";
+
+    /**
+     *
+     */
+    public static final String AT = "@";
+
+    /**
+     *
+     */
+    public static final String PERCENT = "%";
+
+    /**
+     *
+     */
+    public static final String AND = "&";
+
+    /**
+     *
+     */
+    public static final String CARET = "^";
+
+    /**
+     *
+     */
+    public static final String TILDE = "~";
+
+    /**
+     *
+     */
+    public static final String BRACES = "{}";
+
+    /**
+     *
+     */
+    public static final String LEFT_BRACES = "{";
+
+    /**
+     *
+     */
+    public static final String RIGHT_BRACES = "}";
+
+    /**
+     *
+     */
+    public static final String BRACKETS = "[]";
+
+    /**
+     *
+     */
+    public static final String RIGHT_BRACKETS = "[";
+
+    /**
+     *
+     */
+    public static final String LEFT_BRACKETS = "]";
+
+    /**
+     *
+     */
+    public static final String PARENTHESES = "()";
+
+    /**
+     *
+     */
+    public static final String LEFT_PARENTHESES = "(";
+
+    /**
+     *
+     */
+    public static final String RIGHT_PARENTHESES = ")";
+
+    /**
+     *
+     */
+    public static final String ANGULAR_BRACKETS = "<>";
+
+    /**
+     *
+     */
+    public static final String LESS_THAN = "<";
+
+    /**
+     *
+     */
+    public static final String GREATER_THAN = ">";
+
+    /**
+     *
+     */
+    public static final String ACUTE = "´";
+
+    /**
+     *
+     */
+    public static final String GRAVE = "`";
+
+    /**
+     *
+     */
+    public static final String CEDILLA = "ç";
+
+    /**
+     *
+     */
+    public static final String HASH = "#";
+
+    /**
+     *
+     */
+    public static final String SHARP = "#";
+
+    /**
+     *
+     */
+    public static final String POSITIVE = PLUS;
+
+    /**
+     *
+     */
+    public static final String NEGATIVE = MINUS;
+
+    /**
+     *
+     */
+    public static final String PER_MILLE = "‰";
+
+    /**
+     *
+     */
+    public static final String INFINITY = "∞";
+
+    /**
+     *
+     */
+    public static final String VARIES_AS = "∝";
+
+    /**
+     *
+     */
+    public static final String SQUARE_ROOT = "√";
+
+    /**
+     *
+     */
+    public static final String BECAUSE = "∵";
+
+    /**
+     *
+     */
+    public static final String HENCE = "∴";
+
+    /**
+     *
+     */
+    public static final String ELLIPSIS = "…";
+
+    /**
+     *
+     */
+    public static final String PROPORTION = "∷";
+
+    /**
+     *
+     */
+    public static final String TANDEM_COLON = "¨";
+
+    /**
+     *
+     */
+    public static final String ANGLE = "∠";
+
+    /**
+     *
+     */
+    public static final String DITTO = "∶";
+
+    /**
+     *
+     */
+    public static final String SEMICIRCLE = "≲";
+
+    /**
+     *
+     */
+    public static final String PARALLEL = "‖";
+
+    /**
+     *
+     */
+    public static final String VIRGULE = "／";
+
+    /**
+     *
+     */
+    public static final String CIRCUMFERENCE = "○";
+
+    /**
+     *
+     */
+    public static final String SWUNG_DASH = "～";
+
+    /**
+     *
+     */
+    public static final String TRIANGLE = "△";
+
+    /**
+     *
+     */
+    public static final String SECTION = "§";
+
+    /**
+     *
+     */
+    public static final String DIVISION = ";";
+
+    /**
+     *
+     */
+    public static final String PERPENDICULAR = "≱";
+
+    /**
+     *
+     */
+    public static final String ARROW = "→";
+
+    /**
+     *
+     */
+    public static final String UNION = "∪";
+
+    /**
+     *
+     */
+    public static final String INTERSECTION = "∩";
+
+    /**
+     *
+     */
+    public static final String THE_INTEGRAL = "∫";
+
+    /**
+     *
+     */
+    public static final String PLUS_OR_MINUS = "±";
+
+    /**
+     *
+     */
+    public static final String SUMMATION = "∑";
+
+    /**
+     *
+     */
+    public static final String MULTIPLIED = "×";
+
+    /**
+     *
+     */
+    public static final String DEGREE = "°";
+
+    /**
+     *
+     */
+    public static final String DIVIDED = "÷";
+
+    /**
+     *
+     */
+    public static final String MINUTE = "′";
+
+    /**
+     *
+     */
+    public static final String SECOND = "″";
+
+    /**
+     *
+     */
+    public static final String EQUAL_NOT = "≠";
+
+    /**
+     *
+     */
+    public static final String EQUIVALENT = "≡";
+
+    /**
+     *
+     */
+    public static final String CELSIUS_DEGREE = "℃";
+
+    /**
+     *
+     */
+    public static final String EQUAL_APPROXIMATELY = "≌";
+
+    /**
+     *
+     */
+    public static final String ALPHA_UPPER = "Α";
+
+    /**
+     *
+     */
+    public static final String ALPHA_LOWER = "α";
+
+    /**
+     *
+     */
+    public static final String BETA_UPPER = "Β";
+
+    /**
+     *
+     */
+    public static final String BETA_LOWER = "β";
+
+    /**
+     *
+     */
+    public static final String GAMMA_UPPER = "Γ";
+
+    /**
+     *
+     */
+    public static final String GAMMA_LOWER = "γ";
+
+    /**
+     *
+     */
+    public static final String DELTA_UPPER = "Γ";
+
+    /**
+     *
+     */
+    public static final String DELTA_LOWER = "δ";
+
+    /**
+     *
+     */
+    public static final String EPSILON_UPPER = "Δ";
+
+    /**
+     *
+     */
+    public static final String EPSILON_LOWER = "ε";
+
+    /**
+     *
+     */
+    public static final String ZETA_UPPER = "Ε";
+
+    /**
+     *
+     */
+    public static final String ZETA_LOWER = "δ";
+
+    /**
+     *
+     */
+    public static final String ETA_UPPER = "Ζ";
+
+    /**
+     *
+     */
+    public static final String ETA_LOWER = "ε";
+
+    /**
+     *
+     */
+    public static final String THETA_UPPER = "Θ";
+
+    /**
+     *
+     */
+    public static final String THETA_LOWER = "ζ";
+
+    /**
+     *
+     */
+    public static final String IOTA_UPPER = "Η";
+
+    /**
+     *
+     */
+    public static final String IOTA_LOWER = "η";
+
+    /**
+     *
+     */
+    public static final String KAPPA_UPPER = "Κ";
+
+    /**
+     *
+     */
+    public static final String KAPPA_LOWER = "θ";
+
+    /**
+     *
+     */
+    public static final String LAMDA_UPPER = "Λ";
+
+    /**
+     *
+     */
+    public static final String LAMDA_LOWER = "ι";
+
+    /**
+     *
+     */
+    public static final String MU_UPPER = "Μ";
+
+    /**
+     *
+     */
+    public static final String MU_LOWER = "κ";
+
+    /**
+     *
+     */
+    public static final String NU_UPPER = "Ν";
+
+    /**
+     *
+     */
+    public static final String NU_LOWER = "λ";
+
+    /**
+     *
+     */
+    public static final String XI_UPPER = "Ξ";
+
+    /**
+     *
+     */
+    public static final String XI_LOWER = "μ";
+
+    /**
+     *
+     */
+    public static final String OMICRON_UPPER = "Ο";
+
+    /**
+     *
+     */
+    public static final String OMICRON_LOWER = "ν";
+
+    /**
+     *
+     */
+    public static final String PI_UPPER = "Π";
+
+    /**
+     *
+     */
+    public static final String PI_LOWER = "π";
+
+    /**
+     *
+     */
+    public static final String RHO_UPPER = "Ρ";
+
+    /**
+     *
+     */
+    public static final String RHO_LOWER = "ξ";
+
+    /**
+     *
+     */
+    public static final String SIGMA_UPPER = "΢";
+
+    /**
+     *
+     */
+    public static final String SIGMA_LOWER = "ζ";
+
+    /**
+     *
+     */
+    public static final String TAU_UPPER = "Σ";
+
+    /**
+     *
+     */
+    public static final String TAU_LOWER = "η";
+
+    /**
+     *
+     */
+    public static final String UPSILON_UPPER = "Τ";
+
+    /**
+     *
+     */
+    public static final String UPSILON_LOWER = "υ";
+
+    /**
+     *
+     */
+    public static final String PHI_UPPER = "Φ";
+
+    /**
+     *
+     */
+    public static final String PHI_LOWER = "θ";
+
+    /**
+     *
+     */
+    public static final String CHI_UPPER = "Υ";
+
+    /**
+     *
+     */
+    public static final String CHI_LOWER = "χ";
+
+    /**
+     *
+     */
+    public static final String PSI_UPPER = "Φ";
+
+    /**
+     *
+     */
+    public static final String PSI_LOWER = "ψ";
+
+
+}
